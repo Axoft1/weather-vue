60ab9f4954aa3179635fbc4834c5b68eb0689ba4
Aleksei Anufriev <axoft10@gmail.com>
2023-08-17 13:52:37 +0700

    cli

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,17 +1,17 @@
 import {IItems} from '../types'
 
-export const capitalizeFirstLetter = (str) => {
+export const capitalizeFirstLetter = (str:string) => {
     if(!str) return ''
     return str.charAt(0).toUpperCase()+str.slice(1)
 }
 
 const PRESSURE_UNITS = 0.750062
 
-export const getPressureMm = (hpa) => {
+export const getPressureMm = (hpa:number) => {
     return Math.round(hpa * PRESSURE_UNITS);
 }
 
-export const getTime = (second) => {
+export const getTime = (second:number) => {
     return new Date (second * 1000).toLocaleTimeString('ru-RU',{timeZone:'Atlantic/Reykjavik'})
 }
 
@@ -20,18 +20,3 @@ export const onDragStart = (e: DragEvent, item: IItems)=> {
   e.dataTransfer!.effectAllowed = "move"
   e.dataTransfer?.setData('itemId', item.id.toString())
 }
-
-
-// function deleteItem(id: number) {
-//   citys.value = citys.value!.filter((e) => e.id !== id);
-// }
-
-// const citys = ref();
-
-// function addCity(str: string) { 
-//     citys.value.push({ id: 1, name: str }); 
-// }
-
-// watch(citys, () => {
-//   localStorage.setItem("citys", JSON.stringify(citys.value));
-// });
